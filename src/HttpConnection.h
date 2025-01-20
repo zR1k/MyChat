8b1c724f21f545467399de3877d660da4269434f
zR1k <1844130426@qq.com>
2025-01-20 22:13:05 +0800

    beast实现http get请求

---
+++ src/HttpConnection.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "const.h"
+#include <boost/beast.hpp>
+#include <boost/asio.hpp>
+#include <memory>
+
+class LogicSystem; // 前向声明 LogicSystem
+
+
+class HttpConnection: public std::enable_shared_from_this<HttpConnection> {
+    friend class LogicSystem;
+public:
+    HttpConnection(tcp::socket socket);
+    void Start();
+private:
+    void CheckDeadline();
+    void WriteResponse();
+    void HandleReq();
+    tcp::socket  _socket;
+    // The buffer for performing reads.
+    beast::flat_buffer  _buffer{ 8192 };
+    // The request message.
+    http::request<http::dynamic_body> _request;
+    // The response message.
+    http::response<http::dynamic_body> _response;
+    // The timer for putting a deadline on connection processing.
+    net::steady_timer deadline_{
+        _socket.get_executor(), std::chrono::seconds(60) };
+};
